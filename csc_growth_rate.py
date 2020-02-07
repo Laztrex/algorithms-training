fb6bbedf187863a4e59523e0376830b7adecbdbc
Alexey Lazarev <googlaz1@gmail.com>
2020-02-07 20:17:51 +0300

    задачу решил (позже приведу условие). теперь надо оформить код для универсального применения. input - [f*n], output - упорядочить по скорости роста (O-символика)

--- csc_growth_rate.py
+++ csc_growth_rate.py
@@ -1,10 +1,20 @@
-from sympy import *
+from sympy import limit, factorial, log, sqrt
+from sympy.core.numbers import Infinity
 from sympy.abc import *
+import csv
+from collections import defaultdict
 
 
 def lapitale(my_func1, my_func2):
-    return limit(my_func1 / my_func2, z=n, z0='oo')
+    return 1 if isinstance(limit(my_func1 / my_func2, z=n, z0='oo'), Infinity) else 0
 
+
+# def csv_gen(i, j, data):
+#     with open('total.py', 'a', newline='') as out_csv:
+#         writer = csv.DictWriter(out_csv, delimeter=' ', fieldnames=j)
+#         writer.writerow(data)
+#         writer.w
+dict_total = defaultdict(lambda: 0)
 
 func = [("log(log(n, 2))", log(log(n, 2))),
             ("sqrt(log(n, 4))", sqrt(log(n, 4))),
@@ -21,7 +31,7 @@ func = [("log(log(n, 2))", log(log(n, 2))),
             ("n ** sqrt(n)", n ** sqrt(n)),
             ("2 ** n", 2 ** n),
             ("4 ** n", 4 ** n),
-            (" 2 ** (3 * n)", 2 ** (3 * n)),
+            ("2 ** (3 * n)", 2 ** (3 * n)),
             ("factorial(n)", factorial(n))
             ]
 
@@ -29,8 +39,11 @@ num = 0
 for f in func[num:]:
     for i in func:
         if i[0] != f[0]:
-            print(f'{f[0]} | {i[0]} - {lapitale(f[1], i[1])}')
+            a = lapitale(f[1], i[1])
+            dict_total[f[0]] += a
+            print(f'{f[0]} | {i[0]} - {a}')
         else:
             continue
     num += 1
 
+print(dict_total)
